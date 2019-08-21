ddde22d3375a519333ccd930e56216b760dbe8dd
Justin Fuller <justindanielfuller@gmail.com>
2019-08-21 16:11:01 -0400

    Fixing lint errors and warnings

--- lib/Assert.ts
+++ lib/Assert.ts
@@ -1,4 +1,4 @@
-const assert = require('assert')
+import assert from 'assert'
 
 const TYPES = [
   'string',
@@ -11,30 +11,47 @@ const TYPES = [
   'undefined'
 ]
 
+const CALLED_MORE_THAN_ONCE_ERROR =
+  'Expected t.equal to only be called once per test.'
+
+const EQUAL_NOT_CALLED_ERROR = 'Expected t.equal to be called at least once.'
+
+const INVALID_OPTIONS_ERROR =
+  'The only argument of t.equal should be an object.'
+
+const NO_TYPE_EXPECTED_ERROR =
+  'You cannot use a typeof as the expected argument of t.equal().'
+
 export interface AssertOptions {
-  readonly expected: any
-  readonly actual: any
+  readonly expected: any // eslint-disable-line @typescript-eslint/no-explicit-any
+  readonly actual: any // eslint-disable-line @typescript-eslint/no-explicit-any
 }
 
-function AssertOptions (options: AssertOptions) {
-  function isInvalid () {
+interface AssertOptionsValidator {
+  validate(): void | never
+}
+
+function AssertOptionsValidator (
+  options: AssertOptions
+): AssertOptionsValidator {
+  function isInvalid (): boolean {
     return typeof options !== 'object'
   }
 
-  function throwInvalidError () {
-    throw new Error(Assert.INVALID_OPTIONS_ERROR)
+  function throwInvalidError (): never {
+    throw new Error(INVALID_OPTIONS_ERROR)
   }
 
-  function usesTypeofAsExpected () {
+  function usesTypeofAsExpected (): boolean {
     return TYPES.includes(options.expected)
   }
 
-  function throwTypeofAsExpectedError () {
-    throw new Error(Assert.NO_TYPE_EXPECTED_ERROR)
+  function throwTypeofAsExpectedError (): never {
+    throw new Error(NO_TYPE_EXPECTED_ERROR)
   }
 
   return {
-    validate () {
+    validate (): void | never {
       if (isInvalid()) {
         throwInvalidError()
       }
@@ -46,23 +63,28 @@ function AssertOptions (options: AssertOptions) {
   }
 }
 
-export function Assert () {
+interface Assert {
+  throwIfNotCalledAtLeastOnce(): void | never
+  equal(options: AssertOptions): void | never
+}
+
+export function Assert (): Assert {
   let calls = 0
 
-  function throwIfCalledMoreThanOnce () {
+  function throwIfCalledMoreThanOnce (): void | never {
     if (calls > 1) {
-      throw new Error(Assert.CALLED_MORE_THAN_ONCE_ERROR)
+      throw new Error(CALLED_MORE_THAN_ONCE_ERROR)
     }
   }
 
   return {
-    throwIfNotCalledAtLeastOnce () {
+    throwIfNotCalledAtLeastOnce (): void | never {
       if (calls === 0) {
-        throw new Error(Assert.EQUAL_NOT_CALLED_ERROR)
+        throw new Error(EQUAL_NOT_CALLED_ERROR)
       }
     },
-    equal (options: AssertOptions) {
-      AssertOptions(options).validate()
+    equal (options: AssertOptions): void | never {
+      AssertOptionsValidator(options).validate()
 
       calls++
       throwIfCalledMoreThanOnce()
@@ -71,13 +93,10 @@ export function Assert () {
   }
 }
 
-Assert.CALLED_MORE_THAN_ONCE_ERROR =
-  'Expected t.equal to only be called once per test.'
+Assert.CALLED_MORE_THAN_ONCE_ERROR = CALLED_MORE_THAN_ONCE_ERROR
 
-Assert.EQUAL_NOT_CALLED_ERROR = 'Expected t.equal to be called at least once.'
+Assert.EQUAL_NOT_CALLED_ERROR = EQUAL_NOT_CALLED_ERROR
 
-Assert.INVALID_OPTIONS_ERROR =
-  'The only argument of t.equal should be an object.'
+Assert.INVALID_OPTIONS_ERROR = INVALID_OPTIONS_ERROR
 
-Assert.NO_TYPE_EXPECTED_ERROR =
-  'You cannot use a typeof as the expected argument of t.equal().'
+Assert.NO_TYPE_EXPECTED_ERROR = NO_TYPE_EXPECTED_ERROR
