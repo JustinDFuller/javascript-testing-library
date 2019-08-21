d482a9959f7c90a536b6a899faced268bd99abfc
Justin Fuller <justindanielfuller@gmail.com>
2019-08-21 15:39:33 -0400

    Fixing lint errors and warnings

--- lib/Suites.ts
+++ lib/Suites.ts
@@ -10,15 +10,26 @@ interface SuiteOptions {
   paths: string[]
 }
 
-export function Suites (options: SuiteOptions) {
+interface SuiteMeta {
+  suite: Suite
+  path: string
+}
+
+interface Suites {
+  runTests(suites: Suite[]): Promise<void>
+  requireSuites(paths: string[]): SuiteMeta[]
+  runAll(): Promise<void>
+}
+
+export function Suites (options: SuiteOptions): Suites {
   return {
-    async runTests (suites: Suite[]) {
+    async runTests (suites: Suite[]): Promise<void> {
       for (const { suite, path } of suites) {
         options.formatter.emitFile(path)
         await suite.runTests(options.formatter)
       }
     },
-    requireSuites (paths: string[]) {
+    requireSuites (paths: string[]): SuiteMeta[] {
       return paths.map(function (path: string) {
         return {
           suite: require(path).suite,
@@ -26,7 +37,7 @@ export function Suites (options: SuiteOptions) {
         }
       })
     },
-    async runAll () {
+    async runAll (): Promise<void> {
       await this.runTests(this.requireSuites(options.paths))
       options.formatter.end()
     }
