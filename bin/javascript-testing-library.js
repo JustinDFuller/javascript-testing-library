5be0c812534afec94674f6293621ad28d5a595d5
Justin Fuller <justindanielfuller@gmail.com>
2019-09-27 13:10:15 -0400

    CLI

--- bin/javascript-testing-library.js
+++ bin/javascript-testing-library.js
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 
+const meow = require('meow')
 const { main } = require('../dist')
 
-const args = process.argv.slice()
+const cli = meow(`
+  Usage
+    $ javascript-testing-library <input>
 
-main(args[2])
+  Options
+    --watch, -w                 Run tests on changes.
+    --coverage, -c              Run tests in coverage mode.
+    --detect-flakey-tests, -d   Find and report flakey tests.
+    --run-all, -r               Run all tests, don't exit on first failure.
+
+  Examples
+    $ javascript-testing-library lib/**/*.test.ts
+`)
+
+main(cli.input[0])
   .then(res => console.log('done', res))
   .catch(err => console.error('err', err))
